a31e79cefbcb6e0c820113080c1c338ad040a773
Piyatana Kojaranchitt <piyatanakojaranchitt@Piyatanas-MacBook-Air.local>
2025-07-11 05:47:35 +0700

    feat: add initial reroute to login

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,14 +3,15 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: '/api/:path*',
-    //             destination: 'http://localhost:4000/api/:path*', // proxy all /api requests to backend
-    //         },
-    //     ];
-    // },
+    async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
     images: {
         remotePatterns: [
             {
