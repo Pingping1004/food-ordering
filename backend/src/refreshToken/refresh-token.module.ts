efa7e28b2cfd3ae0bef324ecaaeb9d5becd5d336
Piyatana Kojaranchitt <piyatanakojaranchitt@Piyatanas-MacBook-Air.local>
2025-07-11 00:26:14 +0700

    fix: solve nest dependency

--- backend/src/refreshToken/refresh-token.module.ts
+++ backend/src/refreshToken/refresh-token.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RefreshTokenService } from './refresh-token.service'
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+    imports: [PrismaModule],
     providers: [RefreshTokenService],
     exports: [RefreshTokenService],
 })
