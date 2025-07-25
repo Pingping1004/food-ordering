3ea602856485d562d9b8591d56be370988fa0178
nothingelse <96729404+Pingping1004@users.noreply.github.com>
2025-07-25 18:36:04 +0700

    test: add log in menu controller

--- backend/src/menu/menu.controller.ts
+++ backend/src/menu/menu.controller.ts
@@ -75,8 +75,9 @@ export class MenuController {
 
   @Post('bulk')
   async createBulkMenus(@Body() payload: CreateBulkMenusJsonPayload) {
+    console.log('Incoming payload:', JSON.stringify(payload, null, 2));
     this.logger.log(`Req body for bulk menus: `, payload);
-    
+
     if (
       !payload.createMenuDto ||
       !Array.isArray(payload.createMenuDto) ||
