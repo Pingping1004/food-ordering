569cc8c8756ec6975020eca56fb87cd723fde440
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-03-30 11:45:25 +0700

    feat: add command for prod and dev environment

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,16 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const dbUrl = process.env.DATABASE_URL || "";
+
+if (!process.env.NODE_ENV) {
+  throw new Error("APP_ENV is not set");
+}
+
+if (process.env.NODE_ENV === "production") {
+  console.warn("⚠️ Make sure this is the PROD database");
+}
+
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   migrations: {
