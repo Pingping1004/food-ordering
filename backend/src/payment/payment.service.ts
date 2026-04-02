6670f4b21e9a267151dc75e7bf178c3c02f0a8a5
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-02 17:39:14 +0700

    Update payment.service.ts

--- backend/src/payment/payment.service.ts
+++ backend/src/payment/payment.service.ts
@@ -29,7 +29,7 @@ export class PaymentService {
             });
     
             const result = response.data
-            this.logger.debug("Payment account response data: ", response.data.receiver.account.proxy)
+            this.logger.debug("Payment account response data: ", response.data.receiver)
 
             if (result.code !== "200200") throw new HttpException({ message: result.message, code: result.code }, HttpStatus.BAD_REQUEST);
           
