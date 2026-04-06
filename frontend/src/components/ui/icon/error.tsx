c0f9c56fc8a7f4604cf1b0e8cccd7d3a4a8bc845
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-07 00:36:23 +0700

    feat: adjust sizing for icon

--- frontend/src/components/ui/icon/error.tsx
+++ frontend/src/components/ui/icon/error.tsx
@@ -1,6 +1,6 @@
 export function ErrorIcon() {
     return (
-        <svg className="w-6 h-6">
+        <svg viewBox="0 0 24 24" className="w-16 h-16">
             <circle cx="12" cy="12" r="12" className="fill-red-500" />
             <path
                 d="M8 8L16 16M16 8L8 16"
--- frontend/src/components/ui/icon/success.tsx
+++ frontend/src/components/ui/icon/success.tsx
@@ -1,11 +1,11 @@
 export function SuccessIcon() {
     return (
-        <svg className="w-6 h-6">
-            <circle cx="12" cy="12" r="12" className="fill-green-500" />
+        <svg viewBox="0 0 24 24" className="w-16 h-16">
+            <circle cx="12" cy="12" r="11" className="fill-green-500" />
             <path
                 d="M7 12.5L10 15.5L17 8.5"
                 className="stroke-white"
-                strokeWidth="2"
+                strokeWidth="3"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
