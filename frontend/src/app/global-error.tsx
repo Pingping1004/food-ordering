3b5b7bc655c122a4e98157d95a1f8b9b36784cc8
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-21 19:42:45 +0700

    Update global-error.tsx

--- frontend/src/app/global-error.tsx
+++ frontend/src/app/global-error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/Button";
 import { captureException } from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
@@ -10,14 +11,10 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
     }, [error]);
 
     return (
-        <html>
-            <body>
-                {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
-                <NextError statusCode={0} />
-            </body>
-        </html>
+        <div  className="flex flex-col justify-center items-center">
+            <h2>เกิดข้อผิดพลาด</h2>
+            <NextError statusCode={0} />
+            <Button type="button">กลับหน้าหลีก</Button>
+        </div>
     );
 }
