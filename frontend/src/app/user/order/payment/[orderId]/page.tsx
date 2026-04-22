3f54cc12d76beba6b1380fb682960b61abb0218c
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-22 22:51:02 +0700

    fix: condition for duplicate slip err message

--- frontend/src/app/user/order/payment/[orderId]/page.tsx
+++ frontend/src/app/user/order/payment/[orderId]/page.tsx
@@ -188,7 +188,7 @@ function OrderPaymentPage() {
             if (typeof error === 'object' && error !== null && 'response' in error) {
                 const err = error as { response: { status: number; data?: { message?: string, code?: string } } };
 
-                if (err.response.status === 409) {
+                if (err.response.status === 409 && failedCountRef.current >= 1) {
                     toastDanger("ระบบกำลังตรวจสอบสลิป กรุณารอสักครู่");
                     return;
                 }
