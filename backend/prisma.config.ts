a3950366ffd6838ac0eb6fabdf31d50c2564e770
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-26 19:57:05 +0700

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,10 +1,6 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-if (process.env.NODE_ENV === "production") {
-  console.warn("⚠️ Make sure this is the PROD database");
-}
-
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   migrations: {
