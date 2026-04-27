ef22a39a9dcce413b35e6ca9ea13bfdd00d68126
piyatana <96729404+Pingping1004@users.noreply.github.com>
2026-04-28 01:13:31 +0700

    Update page.tsx

--- frontend/src/app/user/order/wait/[orderId]/page.tsx
+++ frontend/src/app/user/order/wait/[orderId]/page.tsx
@@ -139,7 +139,7 @@ function OrderWaitPage() {
     }
     if (!order) return <div>ไม่พบออเดอร์ของคุณ</div>;
 
-    const futureTime = new Date(new Date(order.orderAt).getTime() + 3 * 60 * 1000);
+    const futureTime = new Date(new Date(order.orderAt).getTime() + 4 * 60 * 1000);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-6">
